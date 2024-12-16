91dc88f1e778f72b11b763283a940c63677ab6e9
CeladonTomato70 <diegolunadelabre@gmail.com>
2024-12-15 23:06:31 -0600

    Create CeladonTomato70.py
    Primer ejercicio del repositorio Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/CeladonTomato70.py
@@ -0,0 +1,23 @@
+#Sitio oficial del codigo usado: https://www.python.org/#
+#Comentario en una sola linea#
+'''
+Comentario
+en
+Muntilinea
+'''
+#Crea una variable y una constante(si el lenguaje lo soporta)
+my_new_variable="Mi primera variable"
+pi=3.1416 #Es Constante por conveniencia
+
+#Crea variables representando todos los tipos de datos primitivos
+my_new_string="Mi string"
+my_new_int=19
+my_new_float=1.9
+my_new_bool=True
+
+#Imprime por la terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+print("¡Hola, Python!")
+print(type(my_new_int))
+print(type(my_new_float))
+print(type(my_new_bool))
+print(type(my_new_string))
